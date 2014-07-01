4eb4bbe214bd9650fd20cbf3672cdb9765762982
Armin Ronacher <armin.ronacher@active-4.com>
2014-07-01 10:17:25 +0200

    This is 0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name='pluginbase',
     author='Armin Ronacher',
     author_email='armin.ronacher@active-4.com',
-    version='0.3-dev',
+    version='0.3',
     url='http://github.com/mitsuhiko/pluginbase',
     py_modules=['pluginbase'],
     description='A support library for building plugins sytems in Python.',
