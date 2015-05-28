3c738e5a35371b589d32adc43d299758788f4304
jhermann <jh@web.de>
2015-05-28 13:59:04 +0200

    bring classifiers in sync with tox + Travis, add two topical ones

--- setup.py
+++ setup.py
@@ -16,6 +16,13 @@ setup(
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Environment :: Plugins',
+        'Intended Audience :: Developers',
     ],
 )
