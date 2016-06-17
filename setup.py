283388b0ae5097b6c2df8bced81e394d9ae30a7f
Spencer McIntyre <zeroSteiner@gmail.com>
2016-06-17 08:09:47 -0400

    Add Python 3.5 to the classifiers list

--- setup.py
+++ setup.py
@@ -21,8 +21,9 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
-    ],
+    ]
 )
