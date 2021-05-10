ad876a1282423c5e677392814eec09d826fd311e
Matthew Bakhtiari <dev@mtbk.me>
2021-05-10 05:47:33 +0200

    Add python 3.8 and 3.9 support

--- setup.py
+++ setup.py
@@ -37,6 +37,8 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Environment :: Plugins',
         'Intended Audience :: Developers',
